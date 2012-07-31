3d7f0e2757fa0203c0e66d1fea13286f7dcf15b6
0x55aa <shanqiuchant@gmail.com>
2012-07-31 22:51:05 +0800

    update

--- README.md
+++ README.md
@@ -5,4 +5,9 @@ jQuery-File-Upload 在django上的例子
 
 只是实现了小部分功能：上传，当然强大的jQuery-File-Upload
 以后用到慢慢更新。
+----------------------------
+2012-07-31更新：
+上传进度条，上传图片文件的 大小，格式和上传数量的限制。
+
+
 blog：http://0x55aa.sinaapp.com
--- upload/image/views.py
+++ upload/image/views.py
@@ -9,10 +9,12 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedire
 
 
 
-MAX_FILE_SIZE = 6 * 2 ** 20	# 2 Mb
+MAX_FILE_SIZE = 7 * 2 ** 10	# 2 Mb
 MIX_FILE_SIZE = 3 * 2 ** 10	# 1 Kb
+
 #允许的文件类型
 ACCEPTED_FORMATS = (
+            "image/pjpeg",
             "image/jpeg",
             "image/png",
             )
@@ -29,37 +31,48 @@ def uploadimage(request):
             error = "minFileSize"
         if file.content_type not in ACCEPTED_FORMATS:
             error = "acceptFileTypes"
+        if request.session["is_allow_upload"] == 0:
+            error = "maxNumberOfFiles"
+        else:
+            request.session["is_allow_upload"] -= 1
+                
+        response_data = {
+            "name": file.name,
+            "size": file.size,
+            "type": file.content_type,
+            #"url":'static/'+image.image_url.url,
+            }
+        if error:
+            response_data["error"] = error
+            response_data = simplejson.dumps([response_data])
+            return HttpResponse(response_data, mimetype='text/html')
+
         #保存文件
         image = Images(
             image_url = file,
             )
         image.save()
-        
-        response_data = {
-            "name": file.name,
-            "size": file.size,
-            "type": file.content_type,
-            "url":'static/'+image.image_url.url,
-            }
-        if error:
-            response_data["error"] = error
-            response_data = simplejson.dumps([response_data])
-            return HttpResponse(response_data, mimetype='application/json')
-        
+        response_data["url"] = 'static/'+image.image_url.url
         #删除链接
         response_data["delete_url"] = request.path + "del/" + str(image.id)
 
         response_data["delete_type"] = "GET"
         response_data = simplejson.dumps([response_data])
-        response_type = "application/json"
-        if "text/html" in request.META["HTTP_ACCEPT"]:
-            response_type = "text/html"
+        response_type = "text/html"
+        if "application/json" in request.META["HTTP_ACCEPT"]:
+            response_type = "application/json"
         return HttpResponse(response_data, mimetype=response_type)
+
         
         
     else:
+        #上传图片数量
+        maxnumberoffiles = 3
+        request.session["is_allow_upload"] = maxnumberoffiles
+        print request.session["is_allow_upload"]
         return render_to_response('index.html',
-                    {"open_tv":u'{%',"close_tv": u'%}',"maxfilesize": MAX_FILE_SIZE,"minfilesize": MIX_FILE_SIZE,},
+                    {"open_tv":u'{%',"close_tv": u'%}',"maxfilesize": MAX_FILE_SIZE,
+                     "minfilesize": MIX_FILE_SIZE,"maxnumberoffiles":maxnumberoffiles,},
                           context_instance=RequestContext(request))
     
 def delimage(request,image_id):
